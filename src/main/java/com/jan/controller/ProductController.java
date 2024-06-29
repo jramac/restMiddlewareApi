715e21046a1d0602c3aa916968beada3a29e829d
jramac <jramac7@gmail.com>
2024-06-29 16:28:11 +0200

    Dodana funkcionalnost cacheanja requestova u programsku memoriju

--- src/main/java/com/jan/controller/ProductController.java
+++ src/main/java/com/jan/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.jan.model.dto.ProductSummaryDTO;
 import com.jan.model.product.Product;
 import com.jan.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,6 +19,7 @@ public class ProductController {
     @Autowired
     private ProductRepository productRepository;
     //  /products
+    @Cacheable("productsSummary")
     @GetMapping
     public List<ProductSummaryDTO> getAllProductsSummary() {
         List<Product> products = productRepository.findAll();
@@ -26,26 +28,31 @@ public class ProductController {
                 .collect(Collectors.toList());
     }
     //  /details
+    @Cacheable("products")
     @GetMapping("/details")
     public List<Product> getAllProducts() {
         return productRepository.findAll();
     }
     //  /products/targetId
+    @Cacheable("productId")
     @GetMapping("/{id}")
     public Optional<Product> getProductById(@PathVariable Integer id) {
         return productRepository.findById(id);
     }
     //  /category?category=targetCategory
+    @Cacheable("category")
     @GetMapping("/category")
     public List<Product> getProductsByCategory(@RequestParam String category) {
         return productRepository.findByCategory(category);
     }
     //  /price?price=targetPrice
+    @Cacheable("price")
     @GetMapping("/price")
     public List<Product> getProductsByPrice(@RequestParam Double price) {
         return productRepository.findByPriceLessThanEqual(price);
     }
     // /name?name=targetName
+    @Cacheable("name")
     @GetMapping("/name")
     public List<Product> getProductsByName(@RequestParam String name) {
         return productRepository.findByTitleContainingIgnoreCase(name);
